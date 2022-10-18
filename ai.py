f1c484b2bac4c7765ce959a32fc8177fc9c2f319
W4llnut <111294521+W4llnut@users.noreply.github.com>
2022-10-18 22:54:11 +0200

    Update ai.py

--- ai.py
+++ ai.py
@@ -7,15 +7,15 @@ class AI:
 		key = os.environ['FERNET']
 		fernet = Fernet(key)
 		lines = fernet.decrypt(f.read()).decode("utf-8").split("\r\n")
-		self.bubbles = [[],[],[],[]]
+		self.bubbles = [[],[],[],[],[],[],[],[],[]]
 		line = 0
-		for j in range(4):
+		for j in range(9):
 			for i in range(int(lines[line])):
 				self.bubbles[j].append([float(i) for i in lines[line+1+i].split(" ")])
 			line += int(lines[line])+1
 
 	def eval(self, state):
-		for i in range(4):
+		for i in range(9):
 			flag = 0
 			for j in range(len(self.bubbles[i])):
 				if abs(state[i]-self.bubbles[i][j][0])<=self.bubbles[i][j][1]:
@@ -23,8 +23,3 @@ class AI:
 			if flag == 0:
 				return False
 		return True
-
-
-A = AI()
-
-print(A.eval([1550.272,1551.935,1550.272,1551.59,1551.59,0.0,1549.046,1545.988,5.619,0.764,0.166,1541.726,0,24.446,36.0,62.315]))
